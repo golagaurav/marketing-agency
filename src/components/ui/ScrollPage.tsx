e8150f0fb9d0065ec49483dde0693ed26f683023
golagaurav <ggolaibm@gmail.com>
2025-09-10 14:10:06 +0530

    ui page update

--- src/components/ui/ScrollPage.tsx
+++ src/components/ui/ScrollPage.tsx
@@ -1,27 +1,19 @@
 import * as React from "react";
-import { cn } from "../../lib/utils";
 
-export interface ScrollPageProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export const ScrollPage = React.forwardRef<HTMLDivElement, ScrollPageProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <div
-        className={cn("h-screen overflow-y-scroll bg-gray-100 p-4", className)}
-        ref={ref}
-        {...props}
-      >
-        {[...Array(30).keys()].map((num) => (
-          <div
-            key={num}
-            className="mb-4 p-4 bg-white rounded shadow"
-          >
-            Scroll item #{num + 1}
-          </div>
-        ))}
-      </div>
-    );
-  }
-);
-
-ScrollPage.displayName = "ScrollPage";
+export const ScrollPage = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className="max-h-[400px] overflow-y-scroll p-4 bg-gray-100 rounded"
+      {...props}
+    >
+      {Array.from({ length: 30 }, (_, num) => (
+        <div
+          key={num}
+          className="mb-4 p-4 bg-white rounded shadow"
+        >
+          Scroll item #{num + 1}
+        </div>
+      ))}
+    </div>
+  );
+};

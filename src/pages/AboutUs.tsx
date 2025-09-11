4931d80d4af10f9b4a77fbbb5f8d30fafa1bf0dc
golagaurav <ggolaibm@gmail.com>
2025-09-11 11:53:28 +0530

    woring about

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -7,8 +7,15 @@ const AboutUs = () => {
     <>
       <Navbar />
       <main className="p-6">
-        <h1 className="text-4xl font-bold mb-6">About Us</h1>
-        <p>Information about our company and team.</p>
+        <h1 className="text-4xl text-center font-bold mb-6">About Us</h1>
+        <h1 className="text-3xl font-bold mb-4 text-center">working is process</h1>
+        <div className="flex justify-center mb-16">
+          <img
+            src="https://www.cflowapps.com/wp-content/uploads/2021/06/improve_proceswork.jpg"
+            alt="Process Work"
+            className="max-w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
       </main>
       <Footer />
     </>

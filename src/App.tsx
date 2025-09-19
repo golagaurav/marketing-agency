d38d1fe6e4f3ec1f6aed304875f3f1da66a1a40a
golagaurav <ggolaibm@gmail.com>
2025-09-19 11:03:06 +0530

    remove extra space

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'wouter';
-
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import AboutServices from './pages/AboutServices';
